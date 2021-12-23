2cb0567f3695aa82ee633d0dda263b490201eb2f
Nonthawach Chaisaowong <60466913+nonthawachchaisaowong@users.noreply.github.com>
2021-12-23 19:44:10 +0700

    Initial

---
+++ NETCoreMVCAuthWithIdentity/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NETCoreMVCAuthWithIdentity.Areas.Identity.Data;
+using NETCoreMVCAuthWithIdentity.Data;
+
+[assembly: HostingStartup(typeof(NETCoreMVCAuthWithIdentity.Areas.Identity.IdentityHostingStartup))]
+namespace NETCoreMVCAuthWithIdentity.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<AuthDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AuthDBContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireNonAlphanumeric = false;
+                })
+                .AddEntityFrameworkStores<AuthDBContext>();
+            });
+        }
+    }
+}
